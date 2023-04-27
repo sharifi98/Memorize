f98881b7d57892ed3e8a056049d4749290bd8fdd
Hossein Sharifi <47387238+sharifi98@users.noreply.github.com>
2023-04-27 10:14:09 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Hossein Sharifi on 27/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
