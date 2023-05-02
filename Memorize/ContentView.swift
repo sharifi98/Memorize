861d8e91a8c8735c8931382072337bbeefdf3a8f
Hossein Sharifi <47387238+sharifi98@users.noreply.github.com>
2023-05-02 11:13:16 +0200

    added basic functionality to show 6 different tiles on the screen

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,19 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var emojis: Array<String> = ["🚗", "🚕", "🚙", "🚌", "🚎", "🚓", "🚑", "🚒", "🚐", "🚚", "🚛", "🚜", "🏍", "🚲", "🛴", "🚠", "🚟", "🚡", "🚁", "✈️", "🛩", "🛫", "🛬", "🚀", "🛸", "🛳", "🚤", "⛵️", "🚢", "🚂", "🚆", "🚇", "🚊", "🚉", "🚞", "🚖", "🚍"]
+
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        HStack {
+            ForEach(emojis[0..<6], id: \.self ,content: { emoji in
+                CardView(content: emoji)
+            })
+            
         }
-        .padding()
+        .padding(.horizontal)
+        .foregroundColor(.red)
+        
+    }
+}
+
+struct CardView: View {
+    var content: String
+    @State var isFaceUp: Bool = true
+    
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            } else {
+                shape.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
     }
 }
