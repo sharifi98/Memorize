6bd9394330acfbda84db8d6d3a661d66408f4920
Hossein Sharifi <47387238+sharifi98@users.noreply.github.com>
2023-05-03 13:00:06 +0200

    added more different themes (christmas and fruits) to the card game, each time a new theme is selected the cards are randomised.

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -1,84 +1,97 @@
-//
-//  ContentView.swift
-//  Memorize
-//
-//  Created by Hossein Sharifi on 27/04/2023.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     
-    var emojis: Array<String> = ["🚗", "🚕", "🚙", "🚌", "🚎", "🚓", "🚑", "🚒", "🚐", "🚚", "🚛", "🚜", "🏍", "🚲", "🛴", "🚠", "🚟", "🚡", "🚁", "✈️", "🛩", "🛫", "🛬", "🚀", "🛸", "🛳", "🚤", "⛵️", "🚢", "🚂", "🚆", "🚇", "🚊", "🚉", "🚞", "🚖", "🚍"]
-
-    @State var emojiCount = 20
+    struct Theme {
+        var name: String
+        var emojis: [String]
+    }
+    
+    let themes: [Theme] = [
+        Theme(name: "Vehicles", emojis: ["🚗", "🚕", "🚙", "🚌", "🚎", "🚓", "🚑", "🚒", "🚐", "🚚", "🚛", "🚜", "🏍", "🚲", "🛴", "🚠", "🚟", "🚡", "🚁", "✈️", "🛩", "🛫", "🛬", "🚀", "🛸", "🛳", "🚤", "⛵️", "🚢", "🚂", "🚆", "🚇", "🚊", "🚉", "🚞", "🚖", "🚍"]),
+        Theme(name: "Christmas", emojis: ["🎄", "🎅", "🎁", "⛄️", "🤶", "🔔", "❄️", "🕯️", "🎉", "🦌", "🎶", "🎊", "🎄🎁", "🎅🏼", "🎄🎅🎁", "🎄🕯️"]),
+        Theme(name: "Fruits", emojis: ["🍎", "🍊", "🍌", "🍉", "🍇", "🍓", "🍈", "🍒", "🍑", "🍍", "🥝", "🥥", "🥭", "🍅", "🥑", "🥦", "🥬", "🥒", "🌽"])
+    ]
+    
+    @State private var selectedTheme = 0
+    @State private var chosenTheme: [String] = []
+    @State private var emojiCount = 15
+    
+    init() {
+        chosenTheme = themes[0].emojis
+    }
     
     var body: some View {
-        VStack {
-            ScrollView {
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
-                    ForEach(emojis[0..<emojiCount], id: \.self, content: { emoji in
-                        CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
-                    })
+        NavigationView {
+            VStack {
+                ScrollView {
+                    LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                        ForEach(chosenTheme.prefix(emojiCount).shuffled(), id: \.self, content: { emoji in
+                            CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
+                        })
+                    }
                 }
-            }
-            .foregroundColor(.red)
-            
-            Spacer()
-            HStack{
-                remove
+                .foregroundColor(.red)
+                
                 Spacer()
-                Text("\(emojiCount)")
-                add
+                
+                HStack {
+                    ForEach(0..<themes.count) { index in
+                        VStack {
+                            Button(action: {
+                                selectedTheme = index
+                                chosenTheme = themes[index].emojis
+                            }, label: {
+                                if themes[index].name == "Vehicles" {
+                                    Image(systemName: "car")
+                                } else if themes[index].name == "Christmas" {
+                                    Image(systemName: "snowflake")
+                                } else if themes[index].name == "Fruits" {
+                                    Image(systemName: "apple.logo")
+                                }
+                            })
+                            .frame(maxWidth: .infinity)
+                            
+                            Text(themes[index].name)
+                                .font(.system(size: 17))
+                        }
+                    }
+                }
+                .font(.largeTitle)
+                .padding(.horizontal)
             }
-            .font(.largeTitle)
             .padding(.horizontal)
+            .navigationTitle("Memorize!")
         }
-        .padding(.horizontal)
-
+   
     }
     
-    var remove: some View {
-        Button {
-            if emojiCount > 1 {
-                emojiCount -= 1
+    struct CardView: View {
+        var content: String
+        @State var isFaceUp: Bool = true
+        
+        var body: some View {
+            ZStack {
+                let shape = RoundedRectangle(cornerRadius: 20)
+                if isFaceUp {
+                    shape.fill().foregroundColor(.white)
+                    shape.strokeBorder(lineWidth: 3)
+                    Text(content).font(.largeTitle)
+                } else { shape.fill() }
             }
-        } label: {Image(systemName: "minus.circle")}
-    }
-    
-    var add: some View {
-        Button {
-            if emojiCount < emojis.count {
-                emojiCount += 1
+            .onTapGesture {
+                isFaceUp = !isFaceUp
             }
-        } label: {Image(systemName: "plus.circle")}
+        }
+        
     }
-}
-
-struct CardView: View {
-    var content: String
-    @State var isFaceUp: Bool = true
     
-    var body: some View {
-        ZStack {
-            let shape = RoundedRectangle(cornerRadius: 20)
-            if isFaceUp {
-                shape.fill().foregroundColor(.white)
-                shape.strokeBorder(lineWidth: 3)
-                Text(content).font(.largeTitle)
-            } else { shape.fill() }
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+                .preferredColorScheme(.dark)
+            ContentView()
+                .preferredColorScheme(.light)
         }
-        .onTapGesture {
-            isFaceUp = !isFaceUp
-        }
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-            .preferredColorScheme(.dark)
-        ContentView()
-            .preferredColorScheme(.light)
     }
 }
